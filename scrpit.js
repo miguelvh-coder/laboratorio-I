503888ada4baf520936950cf4d1517100d4b2866
Miguel Velez Higuita <mhiguita@uninorte.edu.co>
2024-02-05 18:17:42 -0500

    este es mi tercer commit

--- scrpit.js
+++ scrpit.js
@@ -1,3 +1,27 @@
-console.log("hola mundo");
+const cantidad = 5
 
-console.log("como estan estrellitas");
+let i = 0 
+let primoTemp = 1
+
+while (i < cantidad){
+
+
+
+}
+
+const esPrimo = primo => {
+
+let cantDivisores=0;
+
+    for(let i=1;i <= primo; i++){
+        if(primo % i ==0){
+            cantDivisores++
+        }
+    }
+
+    if(cantDivisores==2){
+        return true
+    }
+    
+    return false
+}
